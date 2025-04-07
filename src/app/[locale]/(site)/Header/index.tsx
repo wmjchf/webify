c5ec8930918e4f8a1a28aab949ae46182049f32e
wmjchf <3284304056@qq.com>
2025-04-07 21:50:03 +0800

    feat: 图标更新

--- src/app/[locale]/(site)/Header/index.tsx
+++ src/app/[locale]/(site)/Header/index.tsx
@@ -29,7 +29,7 @@ export const Header: React.FC<IHeader> = (props) => {
           <Image
             src="/image/header/logo.png"
             height={40}
-            width={216}
+            width={214}
             alt=""
           ></Image>
         </div>
