db9c3deafa8b3f502ff23263fcbc328af782274f
wmjchf <3284304056@qq.com>
2025-04-07 09:47:03 +0800

    feat: 加载数据

--- src/components/FollowModal/List/index.tsx
+++ src/components/FollowModal/List/index.tsx
@@ -9,6 +9,7 @@ import { NoData } from "../../NoData";
 import { followList } from "../../../service/follow";
 import { IUser } from "../../../service/public";
 import { IAllCollect } from "../../../function/list";
+import { Loading } from "../../Loading";
 
 interface IList {
   type: "1" | "2";
@@ -17,7 +18,7 @@ interface IList {
 }
 export const List: React.FC<IList> = (props) => {
   const { type, uid, allFollowList } = props;
-
+  const [loading, setLoading] = useState(false);
   const pageRef = useRef(0);
   const listRef = useRef<IUser[]>([]);
   const [list, setList] = useState<IUser[] | null>(null);
@@ -34,6 +35,7 @@ export const List: React.FC<IList> = (props) => {
     if (result.code === 200) {
       const newList = [...listRef.current, ...result.data.list];
       setList(newList);
+      setLoading(false);
       listRef.current = newList;
       if (result.data.list.length < 10) {
         setHasMore(false);
@@ -48,36 +50,51 @@ export const List: React.FC<IList> = (props) => {
   };
 
   useEffect(() => {
+    setLoading(true);
     handleGetList();
   }, []);
 
-  return (
-    <div className="h-full" id={type}>
-      {list?.length === 0 ? (
+  const renderList = () => {
+    if (loading) {
+      return (
+        <div className="h-[calc(100%-50px)] flex justify-center items-center">
+          <Loading></Loading>
+        </div>
+      );
+    }
+    if (list?.length === 0) {
+      return (
         <div className="h-[calc(100%-50px)]">
           <NoData></NoData>
         </div>
-      ) : (
-        <InfiniteScroll
-          dataLength={list?.length || 0}
-          hasMore={hasMore}
-          scrollableTarget={type}
-          loader={<></>}
-          next={() => {
-            // handleGetList();
-          }}
-        >
-          {list?.map((item) => {
-            return (
-              <Item
-                allFollowList={allFollowList}
-                key={item.uid}
-                data={item}
-              ></Item>
-            );
-          })}
-        </InfiniteScroll>
-      )}
+      );
+    }
+    return (
+      <InfiniteScroll
+        dataLength={list?.length || 0}
+        hasMore={hasMore}
+        scrollableTarget={type}
+        loader={<></>}
+        next={() => {
+          // handleGetList();
+        }}
+      >
+        {list?.map((item) => {
+          return (
+            <Item
+              allFollowList={allFollowList}
+              key={item.uid}
+              data={item}
+            ></Item>
+          );
+        })}
+      </InfiniteScroll>
+    );
+  };
+
+  return (
+    <div className="h-full" id={type}>
+      {renderList()}
     </div>
   );
 };
--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -1,44 +1,5 @@
-import React from "react";
-import classNames from "classnames";
+import styles from "./index.module.scss";
 
-export type ILoadingProps = {
-  className?: string;
-  style?: React.CSSProperties;
-
-  size?: number;
-  activeColor?: string;
-  noActiveColor?: string;
-  loadingWidth?: number;
+export const Loading = () => {
+  return <div className={styles.loader}></div>;
 };
-
-const Loading: React.FC<ILoadingProps> = (props) => {
-  const {
-    className,
-    style = {},
-    size = 24,
-    activeColor = "#ff3d00",
-    noActiveColor = "#fff",
-    loadingWidth = 2.5,
-  } = props;
-
-  const prefixCls = "ui-loading";
-
-  const classes = classNames(prefixCls, {
-    [`${prefixCls}--default`]: true,
-  });
-
-  return (
-    <span
-      className={classNames(classes, className)}
-      style={{
-        ...style,
-        borderColor: noActiveColor,
-        borderBottomColor: activeColor,
-        width: size,
-        height: size,
-        borderWidth: loadingWidth,
-      }}
-    ></span>
-  );
-};
-export default Loading;
